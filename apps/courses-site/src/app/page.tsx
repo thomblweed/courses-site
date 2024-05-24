235f8111e215d179ae53f30ca9a3831580649b5f
thom <thomblweed@gmail.com>
2024-05-24 20:20:12 +0100

    basic login button with temp action

--- apps/courses-site/src/app/page.tsx
+++ apps/courses-site/src/app/page.tsx
@@ -1,9 +1,20 @@
+import { signIn } from '@/services/auth';
 import styles from './page.module.scss';
 
-export default function Index() {
+export default async function Index() {
   return (
     <div className={styles.page}>
       <h1>Courses</h1>
+      <div>
+        <form
+          action={async () => {
+            'use server';
+            await signIn();
+          }}
+        >
+          <button type="submit">Login</button>
+        </form>
+      </div>
     </div>
   );
 }
