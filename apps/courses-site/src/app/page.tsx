314dee2db54c160e1ff643c5db8f832c5cb20681
thom <thomblweed@gmail.com>
2024-05-24 20:43:18 +0100

    poc signin and signout with github oauth

--- apps/courses-site/src/app/page.tsx
+++ apps/courses-site/src/app/page.tsx
@@ -1,20 +1,41 @@
-import { signIn } from '@/services/auth';
+import { auth, signIn, signOut } from '@/services/auth';
 import styles from './page.module.scss';
 
 export default async function Index() {
+  const session = await auth();
+
   return (
     <div className={styles.page}>
       <h1>Courses</h1>
-      <div>
-        <form
-          action={async () => {
-            'use server';
-            await signIn();
-          }}
-        >
-          <button type="submit">Login</button>
-        </form>
-      </div>
+      {
+        // temp poc code
+        session?.user ? (
+          <>
+            <div>Hi {session.user.name}</div>
+            <div>
+              <form
+                action={async () => {
+                  'use server';
+                  await signOut();
+                }}
+              >
+                <button type="submit">Sign Out</button>
+              </form>
+            </div>
+          </>
+        ) : (
+          <div>
+            <form
+              action={async () => {
+                'use server';
+                await signIn();
+              }}
+            >
+              <button type="submit">Sign In</button>
+            </form>
+          </div>
+        )
+      }
     </div>
   );
 }
