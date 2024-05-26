731b17002a83d5df40c0cca7fc4462038189dcb5
thom <thomblweed@gmail.com>
2024-05-26 09:59:41 +0100

    unit test mocking updates

--- apps/courses-site/specs/HomePage.spec.tsx
+++ apps/courses-site/specs/HomePage.spec.tsx
@@ -3,8 +3,9 @@ import { vi } from 'vitest';
 
 import HomePage from '../src/app/page';
 
+const mockAuth = vi.fn();
 vi.mock('@/services/auth', () => ({
-  auth: () => new Promise((resolve) => resolve({ user: { name: 'Jeff' } })),
+  auth: () => mockAuth(),
 }));
 
 it('should display a heading', async () => {
@@ -16,9 +17,36 @@ it('should display a heading', async () => {
   ).toBeVisible();
 });
 
-it('should display hi message', async () => {
-  const homePageJsx = await HomePage();
-  render(homePageJsx);
+describe('When auth returns a session', () => {
+  beforeEach(() => {
+    mockAuth.mockResolvedValue({ user: { name: 'Jeff' } });
+  });
 
-  expect(screen.getByText('Hi Jeff')).toBeVisible();
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it('should display hi message', async () => {
+    const homePageJsx = await HomePage();
+    render(homePageJsx);
+
+    expect(screen.getByText('Hi Jeff')).toBeVisible();
+  });
+});
+
+describe('When auth returns null', () => {
+  beforeEach(() => {
+    mockAuth.mockResolvedValue(null);
+  });
+
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it('should display sign in button', async () => {
+    const homePageJsx = await HomePage();
+    render(homePageJsx);
+
+    expect(screen.getByRole('button', { name: 'Sign In' })).toBeVisible();
+  });
 });
