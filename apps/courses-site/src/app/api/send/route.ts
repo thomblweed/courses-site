6064488ea05b3a2fb1fe7ccda357fc182513a3e0
thom <thomblweed@gmail.com>
2024-05-26 16:26:28 +0100

    temp api builf fix

--- apps/courses-site/src/app/api/send/route.ts
+++ apps/courses-site/src/app/api/send/route.ts
@@ -1,26 +1,26 @@
-'use server';
 import { Resend } from 'resend';
 
-import { EmailTemplate } from '../components/EmailTemplate';
+import { EmailTemplate } from '@/features/email/components/EmailTemplate';
 
 const API_KEY = process.env.RESEND_API_KEY || 'temp-build-fix-value';
 const resend = new Resend(API_KEY);
 
-const errorResponse = (error: unknown) => ({ error });
+const errorResponse = (error: unknown) =>
+  Response.json({ error }, { status: 500 });
 
-export const sendEmail = async () => {
+export async function POST() {
   try {
     const { data, error } = await resend.emails.send({
       // currently only works with @resend.dev, presumably free tier constraint
       from: 'Courses <onboarding@resend.dev>',
       // works with my email in the "to" property
-      to: ['test@testing.com'],
+      to: ['thomblweed@gmail.com'],
       subject: 'Courses Registration Confirmation',
       react: EmailTemplate({ firstName: 'Jeff' }),
     });
 
-    return error ? errorResponse(error) : data;
+    return error ? errorResponse(error) : Response.json(data);
   } catch (error) {
     return errorResponse(error);
   }
-};
+}
