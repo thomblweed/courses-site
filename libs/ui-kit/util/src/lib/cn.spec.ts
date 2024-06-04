a4a2abf5d765a1ffe3195ee2ca439be8404c40fd
thom <thomblweed@gmail.com>
2024-06-04 07:46:49 +0100

    nx migrations

---
+++ libs/ui-kit/util/src/lib/cn.spec.ts
@@ -0,0 +1,9 @@
+import { ClassValue } from 'clsx';
+import { cn } from './cn';
+
+it('should return correctly concatinated values', () => {
+  const classValues = ['hello', 'hello-again'] satisfies ClassValue[];
+  const response = cn(classValues);
+
+  expect(response).toEqual('hello hello-again');
+});
